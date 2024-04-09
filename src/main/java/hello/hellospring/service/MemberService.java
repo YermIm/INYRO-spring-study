161e6e75ccb5a08565af14a00d74f9a9dfcd6be1
YermIm <iyr7773@naver.com>
2024-04-09 12:19:12 +0900

    2주차 과제
    Signed-off-by: YermIm <iyr7773@naver.com>

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,13 +3,19 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -19,7 +25,6 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-
         validateDuplicateMember(member);  //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
